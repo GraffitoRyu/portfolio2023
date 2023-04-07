df8d3452ce8a87b87e3de05d10e531023b190606
graffitoryu <yth4135@naver.com>
2023-04-07 18:06:16 +0900

    favicon 코드 추가

--- portfolio2023_vite/src/templates/profile/helmet.jsx
+++ portfolio2023_vite/src/templates/profile/helmet.jsx
@@ -5,15 +5,22 @@ import PageHeader from '../../components/common/pageHeader';
 import PageFooter from '../../components/common/pageFooter';
 import Contents from './index';
 
+import { themeStateAtom } from "../../hooks/recoil/theme";
+import { useRecoilValue } from "recoil";
+
 export default function profileHelmet() {
+  const theme = useRecoilValue(themeStateAtom);
+  const metaColor = theme.theme == "dark" ? "#5a5a5a" : "#cccccc";
   return (
     <>
       <Helmet>
         <title>프로필 | 류대현 포트폴리오 :: Front-end Engineer / Web Publisher</title>
+        <meta name="msapplication-TileColor" content={metaColor} />
+        <meta name="theme-color" content={metaColor} />
       </Helmet>
       <PageHeader />
       <Contents />
       <PageFooter />
     </>
-  )
+  );
 }
