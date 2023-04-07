1601939a9144639075e66a36c46ecde7c05985fd
graffitoryu <yth4135@naver.com>
2023-04-07 18:43:44 +0900

    페이지 전환효과 세팅중

--- portfolio2023_vite/src/App.jsx
+++ portfolio2023_vite/src/App.jsx
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom';
 import { RecoilRoot, atom, selector, useRecoilState, useRecoilValue } from 'recoil';
 import { HelmetProvider } from 'react-helmet-async';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 import * as Pages from './templates/pageContents';
 
 import checkScreenSize from './hooks/util/checkScreenSize';
 import windowResizeCheck from './hooks/util/windowResize';
+
+import './scss/page_transition.scss'
 
 export default function app() {
   useEffect(() => {
@@ -18,10 +21,14 @@ export default function app() {
     <RecoilRoot>
       <BrowserRouter>
         <HelmetProvider>
-          <Routes>
-            <Route element={<Pages.Profile />} path="/" />
-            <Route element={<Pages.Projects />} path="/projects" />
-          </Routes>
+          <TransitionGroup className='transition-wrapper'>
+            <CSSTransition key={location.pathname} classNames={'page-move'} timeout={300}>
+              <Routes location={location}>
+                <Route element={<Pages.Profile />} path="/" />
+                <Route element={<Pages.Projects />} path="/projects" />
+              </Routes>
+            </CSSTransition>
+          </TransitionGroup>
         </HelmetProvider>
       </BrowserRouter>
     </RecoilRoot>
