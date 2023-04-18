c4334f85ee0fd5e9805ed5b20bd1c3f79952aa2b
graffitoryu <yth4135@naver.com>
2023-04-18 18:59:31 +0900

    복구

--- portfolio2023_vite/src/components/common/pageVisual.jsx
+++ portfolio2023_vite/src/components/common/pageVisual.jsx
@@ -2,18 +2,17 @@ import React from "react";
 
 export default function pageVisual(props) {
   return (
-    <section className="page-visual w-full parallax-section ">
-      {/* <section className="page-visual around-padding w-full sticky top-0 parallax-frame"> */}
-      <div className="parallax-layer parallax-depth-1 flex items-center sm:items-end  around-padding">
-        <h1 className="page-title">
-          <span className="page-title-text page-title-border-text flex items-center">
-            {props.borderText}
-          </span>
-          <span className="page-title-text page-title-filled-text flex items-center">
-            {props.filledText}
-          </span>
-        </h1>
-      </div>
+    <section className="page-visual around-padding w-full sticky top-0 flex items-center sm:items-end">
+      {/* <div className="parallax-layer parallax-depth-1 flex items-center sm:items-end around-padding"> */}
+      <h1 className="page-title">
+        <span className="page-title-text page-title-border-text flex items-center">
+          {props.borderText}
+        </span>
+        <span className="page-title-text page-title-filled-text flex items-center">
+          {props.filledText}
+        </span>
+      </h1>
+      {/* </div> */}
     </section>
   );
 }
