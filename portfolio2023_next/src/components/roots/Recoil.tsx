3fb4d12bc2b44248b9e6da4810b84f2e62b332cf
graffitoryu <yth4135@naver.com>
2023-05-29 17:20:07 +0900

    recoil 설정 수정

--- portfolio2023_next/src/components/roots/Recoil.tsx
+++ portfolio2023_next/src/components/roots/Recoil.tsx
@@ -1,7 +1,9 @@
 "use client";
 
 import { ReactNode } from "react";
-import { RecoilRoot } from "recoil";
+import { RecoilEnv, RecoilRoot } from "recoil";
+
+RecoilEnv.RECOIL_DUPLICATE_ATOM_KEY_CHECKING_ENABLED = false;
 
 export default function Recoil({ children }: { children: ReactNode }) {
   return <RecoilRoot>{children}</RecoilRoot>;
