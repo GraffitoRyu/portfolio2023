09a6613197c1db6df632bb433eb2f2365915ce79
graffitoryu <yth4135@naver.com>
2023-05-29 17:18:00 +0900

    페이지 전환 효과를 위한 컴포넌트 작업 시작

---
+++ portfolio2023_next/src/components/transCover/TransCover.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { styled } from "styled-components";
+
+// style
+import { flex, position } from "@/styles/styled/mixins";
+
+// util
+import { rem } from "@/util/unit";
+import { useRecoilValue } from "recoil";
+import { pageState, pageStateTypes } from "@/states/page";
+import { serif } from "../../../app/layout";
+
+const TransitionCover = styled.div`
+  ${position({ type: "fixed", left: "0rem", bottom: "0rem", z: 3000 })}
+  width:100%;
+  height: 0;
+  background-color: gray;
+  overflow: clip;
+  transition: height 0.3s;
+  &.loading {
+    height: 100%;
+  }
+`;
+
+const TransBox = styled.div`
+  ${position({ bottom: "0rem", left: "0rem" })}
+  ${flex({ std: "start" })}
+  width: 100%;
+  height: ${`${window.innerHeight}px`};
+  padding: 0 ${rem(80)};
+`;
+
+const TransTitle = styled.h1`
+  font-size: ${rem(160)};
+  font-weight: 500;
+  font-family: var(--serif-kr);
+  letter-spacing: 0;
+  color: #333;
+  text-transform: capitalize;
+`;
+
+export default function TransCover() {
+  const { cur } = useRecoilValue<pageStateTypes>(pageState);
+  const [loading, setLoading] = useState("");
+
+  useEffect(() => {
+    setLoading("loading");
+    setTimeout(() => {
+      setLoading("");
+    }, 3000);
+  }, [cur]);
+
+  return (
+    <TransitionCover className={loading}>
+      <TransBox>
+        <TransTitle>{cur}</TransTitle>
+      </TransBox>
+    </TransitionCover>
+  );
+}
