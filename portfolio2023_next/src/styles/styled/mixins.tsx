e933f04db63d069a72320f041d9ebe9a294c5f43
graffitoryu <yth4135@naver.com>
2023-05-29 17:15:09 +0900

    styled mixin jsdoc 추가

--- portfolio2023_next/src/styles/styled/mixins.tsx
+++ portfolio2023_next/src/styles/styled/mixins.tsx
@@ -7,11 +7,18 @@ type SizeTypes = {
   width?: number | string;
   height?: number | string;
 };
-
+/**
+ * @property {number | string} [width] : auto(default), string(%, px, rem 등 단위가 있는 것으로 간주), number는 rem 변환
+ * @property {number | string} [height] : auto(default), string(%, px, rem 등 단위가 있는 것으로 간주), number는 rem 변환
+ */
 export const size = ({ width, height }: SizeTypes) => css`
   width: ${typeof width !== "undefined" ? getUnit(width) : "auto"};
   height: ${typeof height !== "undefined" ? getUnit(height) : "auto"};
 `;
+/**
+ * @property {number | string} [width] : auto(default), string(%, px, rem 등 단위가 있는 것으로 간주), number는 rem 변환
+ * @property {number | string} [height] : auto(default), string(%, px, rem 등 단위가 있는 것으로 간주), number는 rem 변환
+ */
 export const maxSize = ({ width, height }: SizeTypes) => css`
   max-width: ${typeof width !== "undefined" ? getUnit(width) : "auto"};
   max-height: ${typeof height !== "undefined" ? getUnit(height) : "auto"};
@@ -22,6 +29,11 @@ type FlexTypes = {
   std?: string | undefined;
   cross?: string | undefined;
 };
+/**
+ * @property {string | undefined} [dir] : row, column
+ * @property {string | undefined} [std] : justify-content (기준 축 정렬)
+ * @property {string | undefined} [cross] : align-items (교차 축 정렬)
+ */
 export const flex = ({ dir, std, cross }: FlexTypes) => css`
   display: flex;
   justify-content: ${std ?? "center"};
@@ -35,8 +47,18 @@ type PositionTypes = {
   left?: string | undefined;
   bottom?: string | undefined;
   right?: string | undefined;
+  z?: number | undefined;
   center?: boolean | undefined;
 };
+/**
+ * @property {string | undefined} [type] : absolute(default), relative, fixed, sticky, static, ...
+ * @property {string | undefined} [top] : px, rem 단위를 붙인 string 형태 필요
+ * @property {string | undefined} [left] : px, rem 단위를 붙인 string 형태 필요
+ * @property {string | undefined} [bottom] : px, rem 단위를 붙인 string 형태 필요
+ * @property {string | undefined} [right] : px, rem 단위를 붙인 string 형태 필요
+ * @property {number | undefined} [z] : z-index
+ * @property {boolean | undefined} [center] : px, rem 단위를 붙인 string 형태 필요
+ */
 export const position = ({
   type,
   top,
@@ -44,12 +66,14 @@ export const position = ({
   bottom,
   right,
   center,
+  z,
 }: PositionTypes) => css`
   position: ${type ?? "absolute"};
-  ${top ? `top:${top}` : ""};
-  ${left ? `left:${left}` : ""};
-  ${bottom ? `bottom:${bottom}` : ""};
-  ${right ? `right:${right}` : ""};
+  ${top ? `top:${top};` : ""};
+  ${left ? `left:${left};` : ""};
+  ${bottom ? `bottom:${bottom};` : ""};
+  ${right ? `right:${right};` : ""};
+  ${z !== undefined ? `z-index:${z};` : ""}
   ${center
     ? `
     top:50%;
