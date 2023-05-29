35984515de5786459f155fe5cfac0b5db818e595
graffitoryu <yth4135@naver.com>
2023-05-29 17:04:50 +0900

    font 파일 분리, root layout 수정

--- portfolio2023_next/app/layout.tsx
+++ portfolio2023_next/app/layout.tsx
@@ -1,37 +1,25 @@
 // meta data
 export { metadata } from "@/data/meta";
 
-// fonts
-import { Noto_Sans_KR, Noto_Serif_KR } from "next/font/google";
-
 // common template
-import PageTemplate from "@/templates/PageTemplate";
+import PageTemplate from "@/components/_templates/PageTemplate";
 
 // components
 import Recoil from "@/components/roots/Recoil";
 import ReactQuery from "@/components/roots/ReactQuery";
 import ThemeColors from "@/components/roots/ThemeColor";
 import Cursor from "@/components/cursor/Cursor";
-import PageHeader from "@/components/pageHeader/PageHeader";
-
-// fonts
-export const sans = Noto_Sans_KR({
-  weight: ["300", "400", "500", "700", "900"],
-  subsets: ["latin"],
-  variable: "--sans-kr",
-  display: "swap",
-});
-export const serif = Noto_Serif_KR({
-  weight: ["400", "500", "700"],
-  subsets: ["latin"],
-  variable: "--serif-kr",
-  display: "swap",
-});
 
 // styles
 import StyledComponentsRegistry from "./lib/registry";
 import "@/styles/scss/globals.scss";
 import { GlobalBodyStyle } from "@/styles/styled/body";
+import dynamic from "next/dynamic";
+import { sans, serif } from "@/styles/fonts/fonts";
+
+const TransCover = dynamic(() => import("@/components/transCover/TransCover"), {
+  ssr: false,
+});
 
 export default function RootLayout({
   children,
@@ -40,15 +28,16 @@ export default function RootLayout({
 }) {
   return (
     <StyledComponentsRegistry>
-      <html lang="ko">
+      <html lang="ko" className={`${sans.variable} ${serif.variable}`}>
+        <head></head>
         <body>
           <Recoil>
             <ReactQuery>
               <ThemeColors>
                 <GlobalBodyStyle />
-                <div className="page-container relative">
-                  <PageHeader />
+                <div className="page-container w-full h-full relative">
                   <PageTemplate>{children}</PageTemplate>
+                  {/* <TransCover /> */}
                   <Cursor />
                 </div>
               </ThemeColors>
---
+++ portfolio2023_next/src/styles/fonts/fonts.ts
@@ -0,0 +1,14 @@
+import { Noto_Sans_KR, Noto_Serif_KR } from "next/font/google";
+
+export const sans = Noto_Sans_KR({
+  weight: ["300", "400", "500", "700", "900"],
+  subsets: ["latin"],
+  variable: "--sans-kr",
+  display: "swap",
+});
+export const serif = Noto_Serif_KR({
+  weight: ["400", "500", "700"],
+  subsets: ["latin"],
+  variable: "--serif-kr",
+  display: "swap",
+});
