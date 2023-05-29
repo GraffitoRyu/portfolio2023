3e04207a6b9965630797210ba3179b518ed22c78
graffitoryu <yth4135@naver.com>
2023-05-29 17:16:44 +0900

    template 수정 (헤더 포함 등)

--- portfolio2023_next/src/components/_templates/PageTemplate.tsx
+++ portfolio2023_next/src/components/_templates/PageTemplate.tsx
@@ -1,10 +1,12 @@
 import { ReactNode } from "react";
+import PageHeader from "@/components/pageHeader/PageHeader";
 import PageFooter from "@/components/pageFooter/PageFooter";
 
 export default function PageTemplate({ children }: { children: ReactNode }) {
   return (
     <>
-      <div className="scroll-container">
+      <PageHeader />
+      <div className="scroll-container w-full h-full overflow-x-hidden overflow-y-auto">
         {children}
         <PageFooter />
       </div>
