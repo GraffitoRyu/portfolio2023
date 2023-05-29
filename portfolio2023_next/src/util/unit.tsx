04601fee4c8761e598bf81a0b304dbdc04656423
graffitoryu <yth4135@naver.com>
2023-05-29 17:15:26 +0900

    단위변환 함수 jsdoc 추가

--- portfolio2023_next/src/util/unit.tsx
+++ portfolio2023_next/src/util/unit.tsx
@@ -1,7 +1,16 @@
 // px값을 rem으로 변환
 // 시안 기준을 2560px너비 100%로 함
-export const rem: (px: number) => string = px => `${px / 25.6}rem`;
+/**
+ * @property {number} px : 픽셀 단위의 number
+ * @return {string} rem 변환된 단위 값
+ */
+export function rem(px: number): string {
+  return `${px / 25.6}rem`;
+}
 
-// rem 또는 % 단위
+/**
+ * @property {number | string} v : number인 경우 rem 변환, string인 경우 단위가 붙은 것으로 간주하고 그대로 return
+ * @return {string} 단위 변환 값
+ */
 export const getUnit: (v: number | string) => number | string = v =>
   typeof v === "string" ? v : rem(v);
