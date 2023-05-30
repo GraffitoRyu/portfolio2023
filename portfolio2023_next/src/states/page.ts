0d2b84cbbf6f88b964a8041b0f780a8a73a565f9
graffitoryu <yth4135@naver.com>
2023-05-30 16:54:07 +0900

    상태 업데이트

--- portfolio2023_next/src/states/page.ts
+++ portfolio2023_next/src/states/page.ts
@@ -4,6 +4,7 @@ export type pageStateTypes = {
   init: boolean;
   cur: string;
   loaded: boolean;
+  notFound: boolean;
 };
 
 export const pageState = atom<pageStateTypes>({
@@ -12,5 +13,6 @@ export const pageState = atom<pageStateTypes>({
     init: false, // 첫 진입 여부
     cur: "profile", // 현재페이지
     loaded: false, // 로딩 완료 여부
+    notFound: false, // 404 page
   },
 });
