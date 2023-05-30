599eb867ed7f0ffcf394f4a8bfe58a163149e880
graffitoryu <yth4135@naver.com>
2023-05-30 20:08:43 +0900

    footer를 위한 타입 추가

--- portfolio2023_next/src/data/sitemap.ts
+++ portfolio2023_next/src/data/sitemap.ts
@@ -1,10 +1,28 @@
 export type SitemapType = {
+  [index: string]: string | boolean;
   code: string;
   name: string;
   path: string;
   header: boolean;
   contact: boolean;
   external: boolean;
+};
+
+export type SitemapStateTypes = {
+  [index: string]: string;
+  profile: string;
+  projects: string;
+  github: string;
+  notion: string;
+};
+export type ContactStateTypes = {
+  [index: string]: string;
+  jumpit: string;
+  wanted: string;
+  saramin: string;
+  jobkorea: string;
+  rocketpunch: string;
+  kakao: string;
 };
 
 export const sitemapData: SitemapType[] = [
