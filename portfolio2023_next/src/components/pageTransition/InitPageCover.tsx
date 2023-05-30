41cbfd7c78bac09e12a846fd8d561d069f22035a
graffitoryu <yth4135@naver.com>
2023-05-30 16:54:45 +0900

    전환 효과 작업중

---
+++ portfolio2023_next/src/components/pageTransition/InitPageCover.tsx
@@ -0,0 +1,8 @@
+export default function InitPageCover() {
+  return (
+    <div className="intro-container">
+      <div className="title">LOADING</div>
+      <div className="loading"></div>
+    </div>
+  );
+}
