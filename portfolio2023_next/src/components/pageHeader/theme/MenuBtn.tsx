113db6b1975df2daa1f15c8a1a1b227f25a98723
graffitoryu <yth4135@naver.com>
2023-05-31 19:59:06 +0900

    테마 선택 버튼 오류 수정

---
+++ portfolio2023_next/src/components/pageHeader/theme/MenuBtn.tsx
@@ -0,0 +1,94 @@
+import { useState } from "react";
+import { useRecoilState } from "recoil";
+import { styled } from "styled-components";
+
+// state
+import { ThemeStateTypes, themeState } from "@/states/theme";
+
+// style
+import { img } from "@/styles/styled/img";
+import { SvgFill, flex, size } from "@/styles/styled/mixins";
+
+// util
+import { getSystemTheme } from "@/util/changeTheme";
+import { rem } from "@/util/unit";
+
+// SVG
+import * as ThemeSvg from "./BtnIcons";
+function ThemeIcon(theme: string) {
+  switch (theme) {
+    case "light":
+      return <ThemeSvg.Light />;
+    case "dark":
+      return <ThemeSvg.Dark />;
+    case "system":
+      return <ThemeSvg.System />;
+  }
+}
+
+const ThemeMenuButton = styled.button`
+  figure {
+    ${size({ width: rem(56) })}
+    font-size:0;
+    ${flex({})}
+    ${({ theme }) => SvgFill(theme.gnbThemeMenu.menu)}
+    svg {
+      ${img({ width: 24, height: 24 })}
+    }
+  }
+  span {
+    font-size: ${rem(16)};
+    color: ${({ theme }) => theme.gnbThemeMenu.menu};
+    line-height: 1em;
+    margin-top: ${rem(-2)};
+  }
+  &:not(.selected).hover {
+    figure {
+      ${({ theme }) => SvgFill(theme.gnbThemeMenu.hover)}
+    }
+    span {
+      color: ${({ theme }) => theme.gnbThemeMenu.hover};
+    }
+  }
+  &.selected {
+    figure {
+      ${({ theme }) => SvgFill(theme.gnbThemeMenu.selected)}
+    }
+    span {
+      color: ${({ theme }) => theme.gnbThemeMenu.selected};
+    }
+  }
+`;
+
+export default function ThemeMenuBtn({ code }: { code: string }) {
+  const [theme, setTheme] = useRecoilState<ThemeStateTypes>(themeState);
+  const [hover, setHover] = useState("");
+
+  const changeTheme: (selectedTheme: string) => void = selectedTheme => {
+    if (!selectedTheme) return;
+    setTheme(prev => ({
+      ...prev,
+      isSystem: selectedTheme == "system",
+      theme: selectedTheme == "system" ? getSystemTheme() : selectedTheme,
+    }));
+  };
+
+  const updateSelected: (code: string) => string = code => {
+    if (theme.isSystem) return code === "system" ? "selected" : "";
+    return theme.theme === code ? "selected" : "";
+  };
+
+  return (
+    <ThemeMenuButton
+      className={`w-full h-full flex items-center ${updateSelected(
+        code
+      )} ${hover}`}
+      onClick={() => changeTheme(code)}
+      onMouseEnter={() => setHover("hover")}
+      onMouseLeave={() => setHover("")}
+    >
+      <figure>{ThemeIcon(code)}</figure>
+      <span className="capitalize">{code}</span>
+    </ThemeMenuButton>
+  );
+}
--- portfolio2023_next/src/components/pageHeader/theme/MenuList.tsx
+++ portfolio2023_next/src/components/pageHeader/theme/MenuList.tsx
@@ -1,109 +1,30 @@
 import { useEffect, useState } from "react";
-import { useRecoilState } from "recoil";
+import { useRecoilValue } from "recoil";
 import { styled } from "styled-components";
+
+import ThemeMenuBtn from "./MenuBtn";
 
 // state
 import { ThemeStateTypes, themeState } from "@/states/theme";
 
 // util
 import { rem } from "@/util/unit";
-import { applyTheme, getSystemTheme } from "@/util/changeTheme";
+import { applyTheme } from "@/util/changeTheme";
 
-// style
-import { flex, size, SvgFill } from "@/styles/styled/mixins";
-import { img } from "@/styles/styled/img";
-
-// SVG
-import * as ThemeSvg from "./BtnIcons";
-function ThemeIcon(theme: string) {
-  switch (theme) {
-    case "light":
-      return <ThemeSvg.Light />;
-    case "dark":
-      return <ThemeSvg.Dark />;
-    case "system":
-      return <ThemeSvg.System />;
-  }
-}
-
-const menuHeight: string = rem(56);
 const ThemeMenuContainer = styled.div`
   width: ${rem(160)};
   transform: ${`translateY(${rem(16)})`};
   background: ${({ theme }) => theme.gnbThemeMenu.container};
 `;
 const ThemeListItem = styled.li`
-  height: ${menuHeight};
+  height: ${rem(56)};
 `;
-const ThemeMenuBtn = styled.button`
-  figure {
-    ${size({ width: menuHeight })}
-    font-size:0;
-    ${flex({})}
-    ${({ theme }) => SvgFill(theme.gnbThemeMenu.menu)}
-    svg {
-      ${img({ width: 24, height: 24 })}
-    }
-  }
-  span {
-    font-size: ${rem(16)};
-    color: ${({ theme }) => theme.gnbThemeMenu.menu};
-    line-height: 1em;
-    margin-top: ${rem(-2)};
-  }
-  &:not(.selected).hover {
-    figure {
-      ${({ theme }) => SvgFill(theme.gnbThemeMenu.hover)}
-    }
-    span {
-      color: ${({ theme }) => theme.gnbThemeMenu.hover};
-    }
-  }
-  &.selected {
-    figure {
-      ${({ theme }) => SvgFill(theme.gnbThemeMenu.selected)}
-    }
-    span {
-      color: ${({ theme }) => theme.gnbThemeMenu.selected};
-    }
-  }
-`;
-
-type HoverTypes = {
-  [key: string]: string | undefined;
-  light?: string;
-  dark?: string;
-  system?: string;
-};
 
 export default function ThemeMenuList() {
   const themeList: string[] = ["light", "dark", "system"];
-  const themeClasses: HoverTypes =
-    Object.fromEntries(themeList.map(t => [t, ""])) ?? {};
-
-  const [theme, setTheme] = useRecoilState<ThemeStateTypes>(themeState);
+  const theme = useRecoilValue<ThemeStateTypes>(themeState);
   const [openClass, setOpenClass] = useState<string>("hidden");
   const [themeClass, setThemeClass] = useState<string>("system");
-  const [hover, setHover] = useState<HoverTypes>(themeClasses);
-
-  const changeTheme: (selectedTheme: string) => void = selectedTheme => {
-    if (!selectedTheme) return;
-    setTheme(prev => ({
-      ...prev,
-      isSystem: selectedTheme == "system",
-      theme: selectedTheme == "system" ? getSystemTheme() : selectedTheme,
-    }));
-  };
-
-  const updateSelected: (code: string) => string = code => {
-    if (theme.isSystem) return code === "system" ? "selected" : "";
-    return theme.theme === code ? "selected" : "";
-  };
-
-  // useEffect(() => {
-  //   applyTheme(theme.theme);
-  //   setThemeClass(theme.isSystem ? `system-${theme.theme}` : theme.theme);
-  // }, []);
 
   useEffect(() => {
     setOpenClass(theme.isOpen ? "" : "hidden");
@@ -124,19 +45,7 @@ export default function ThemeMenuList() {
             key={`themeMenu_${code}_${Math.floor(Math.random() * 100000)}_${i}`}
             className="w-full"
           >
-            <ThemeMenuBtn
-              className={`w-full h-full flex items-center ${updateSelected(
-                code
-              )} ${hover[code]}`}
-              onClick={() => changeTheme(code)}
-              onMouseEnter={() =>
-                setHover(prev => ({ ...prev, [code]: "hover" }))
-              }
-              onMouseLeave={() => setHover(prev => ({ ...prev, [code]: "" }))}
-            >
-              <figure>{ThemeIcon(code)}</figure>
-              <span className="capitalize">{code}</span>
-            </ThemeMenuBtn>
+            <ThemeMenuBtn code={code} />
           </ThemeListItem>
         ))}
       </ul>
