4ae462d8778e47e02507116645f498e7dca69106
graffitoryu <yth4135@naver.com>
2023-06-04 19:05:52 +0900

    페이지 접속 체크 코드 개선

--- portfolio2023_next/src/hooks/PageLoadEvents.tsx
+++ portfolio2023_next/src/hooks/PageLoadEvents.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useRef } from "react";
-import { usePathname } from "next/navigation";
+import { useParams, usePathname } from "next/navigation";
 import { useRecoilValue, useSetRecoilState } from "recoil";
 
 // state
@@ -16,8 +16,9 @@ import { sitemapData } from "@/data/sitemap";
 const routeData: SitemapType[] = sitemapData.filter(route => !route.external);
 
 export function PageLoadEvents() {
+  const { category } = useParams();
   const pathname = usePathname(); // 현재 루트 수신
-  const savedPathNameRef = useRef<string>(pathname); // 현재 루트 저장
+  const savedPathNameRef = useRef<string>(pathExceptParams(pathname, category)); // 현재 루트 저장
   const setPage = useSetRecoilState<pageStateTypes>(pageState);
 
   // debug
@@ -25,26 +26,38 @@ export function PageLoadEvents() {
 
   useEffect(() => {
     if (!page.init) {
-      console.log(`페이지 최초 로드 완료`);
+      console.log(
+        `페이지 최초 로드 완료: `,
+        pathExceptParams(pathname, category)
+      );
       setPage(prev => ({ ...prev, init: true }));
     }
-  }, [page.init, setPage]);
+  }, [category, page.init, pathname, setPage]);
 
   useEffect(() => {
     // 루트 업데이트
-    if (savedPathNameRef.current !== pathname) {
-      const curPage = getCurPageName(pathname, routeData);
+    if (savedPathNameRef.current !== pathExceptParams(pathname, category)) {
+      const curPage = getCurPageName(
+        pathExceptParams(pathname, category),
+        routeData
+      );
       console.log(`페이지 변경: `, curPage);
       setPage(prev => ({ ...prev, loaded: true, cur: curPage }));
 
-      savedPathNameRef.current = pathname;
+      savedPathNameRef.current = pathExceptParams(pathname, category);
     }
-  }, [page.cur, pathname, setPage]);
+  }, [category, page.cur, pathname, setPage]);
 
   return null;
 }
 
 // 경로에서 페이지 이름 찾기
 export function getCurPageName(curPath: string, data: SitemapType[]) {
-  return data.filter(d => d.path === curPath)[0].code;
+  return data.filter(d => d.path === curPath)[0]?.code;
+}
+
+export function pathExceptParams(path: string, category: string | null) {
+  if (category)
+    return path.includes(category) ? path.replace(`/${category}`, "") : path;
+  return path;
 }
