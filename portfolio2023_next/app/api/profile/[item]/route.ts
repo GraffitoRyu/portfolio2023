564ab7075ef0205ebbde781091d5ed0b8716e797
graffitoryu <yth4135@naver.com>
2023-06-05 01:16:22 +0900

    api 공부중인 코드 주석처리

--- portfolio2023_next/app/api/profile/[item]/route.ts
+++ portfolio2023_next/app/api/profile/[item]/route.ts
@@ -1,6 +1,6 @@
-import { redirect } from "next/navigation";
+// import { redirect } from "next/navigation";
 
-export async function GET({ params }: { params: { item: string } }) {
-  const { item } = params;
-  redirect(`${process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL}/${item}.json`);
-}
+// export async function GET({ params }: { params: { item: string } }) {
+//   const { item } = params;
+//   redirect(`${process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL}/${item}.json`);
+// }
