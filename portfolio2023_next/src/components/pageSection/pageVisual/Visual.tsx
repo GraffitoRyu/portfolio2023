d2a1d39aad8844a9eebb791405431a667b347d9e
graffitoryu <yth4135@naver.com>
2023-06-07 02:29:53 +0900

    gsap scrolltrigger 공부중

--- portfolio2023_next/src/components/pageSection/pageVisual/Visual.tsx
+++ portfolio2023_next/src/components/pageSection/pageVisual/Visual.tsx
@@ -1,3 +1,9 @@
+"use client";
+
+import { useEffect, useRef } from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
 // style components
 import {
   VisualContainer,
@@ -6,9 +12,35 @@ import {
 } from "@/styles/styled/components/PageVisual";
 
 export default function PageVisual({ title }: { title: string[] }) {
+  const visualRef = useRef<HTMLDivElement>(null);
+  const visualTitleRef = useRef<HTMLHeadingElement>(null);
+
+  useEffect(() => {
+    const ctx = gsap.context(() => {
+      gsap.registerPlugin(ScrollTrigger);
+      const timeline = gsap.timeline({
+        defaults: { duration: 1 },
+        scrollTrigger: {
+          trigger: visualRef.current,
+          scrub: true,
+          // pin: true,
+          start: "top top",
+          end: "bottom bottom",
+          markers: true,
+          onUpdate: self => {
+            console.log(self);
+          },
+        },
+      });
+    });
+    return () => {
+      ctx.revert();
+    };
+  }, [visualRef, visualTitleRef]);
+
   return (
-    <VisualContainer>
-      <VisualTitle>
+    <VisualContainer ref={visualRef}>
+      <VisualTitle ref={visualTitleRef}>
         {title.map((t: string) => (
           <VisualTitleLine
             key={`visualTitle_${Math.floor(Math.random() * 1000)}_${t}`}
