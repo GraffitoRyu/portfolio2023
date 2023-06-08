85db63c016e67c23cf137dea52a4cdc19a32afb5
graffitoryu <yth4135@naver.com>
2023-06-08 13:46:54 +0900

    react query devtool 주석처리

--- portfolio2023_next/src/components/roots/ReactQuery.tsx
+++ portfolio2023_next/src/components/roots/ReactQuery.tsx
@@ -2,7 +2,7 @@
 
 import { ReactNode } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+// import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -20,7 +20,7 @@ export default function ReactQuery({ children }: { children: ReactNode }) {
   return (
     <QueryClientProvider client={queryClient}>
       {children}
-      <ReactQueryDevtools initialIsOpen={true} position="bottom-right" />
+      {/* <ReactQueryDevtools initialIsOpen={true} position="bottom-right" /> */}
     </QueryClientProvider>
   );
 }
