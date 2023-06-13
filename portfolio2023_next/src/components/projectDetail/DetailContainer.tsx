173e441db0001197b0f2528a5e32c5989fbf2e9a
graffitoryu <yth4135@naver.com>
2023-06-13 13:58:25 +0900

    프로젝트 상세 container 상태 관리 코드 개선 - 열기/닫기 코드 수정 - 데이터 오류 시 코드 제거 - 언마운트 시 스크롤 ref 초기화

--- portfolio2023_next/src/components/projectDetail/DetailContainer.tsx
+++ portfolio2023_next/src/components/projectDetail/DetailContainer.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useRouter, useSearchParams } from "next/navigation";
+import { useSearchParams } from "next/navigation";
 import { useEffect, useState, useRef } from "react";
 import { useRecoilState, useSetRecoilState } from "recoil";
 
@@ -33,7 +33,6 @@ import { transTime } from "@/styles/styled/preset/transTime";
 import useGetDetailByCodeQuery from "@/hooks/useGetDetailQuery";
 
 export default function ProjectDetail() {
-  const router = useRouter();
   // 프로젝트 코드
   const params = useSearchParams();
   const code = params.get("code");
@@ -50,47 +49,9 @@ export default function ProjectDetail() {
 
   // 프로젝트 데이터 상태관리
   const setDetailData = useSetRecoilState<DetailTypes>(detailData);
-  const { isError, status, data } = useGetDetailByCodeQuery(code);
+  const { status, data } = useGetDetailByCodeQuery(code);
 
-  // 프로젝트 상세 오픈 ; 열림 상태 클래스명으로 컨트롤
-  useEffect(() => {
-    // 프로젝트 클릭 선택으로 여는 경우
-    setOpen(layoutState.open ? "open" : "");
-
-    // url로 직접 접근할 때
-    if (!layoutState.open && code) {
-      setLayoutState(prev => ({ ...prev, open: true }));
-      setOpen("open");
-    }
-  }, [code, layoutState, setLayoutState]);
-
-  // 상세 페이지 오픈 슬라이드 완료 상태 업데이트
-  useEffect(() => {
-    if (code && open === "open") {
-      const timer = setTimeout(() => {
-        setLayoutState(prev => ({ ...prev, openComplete: true }));
-      }, transTime.detail.sheetSlideTime);
-
-      return () => clearTimeout(timer);
-    }
-  }, [code, open, setLayoutState]);
-
-  // 데이터 조회 오류 시, 창 닫기
-  useEffect(() => {
-    // 데이터 조회 실패 시 닫기
-    if (isError) {
-      setLayoutState(prev => ({ ...prev, open: false, openComplete: false }));
-      router.back();
-    }
-  }, [isError, router, setLayoutState]);
-
-  // 스크롤 참조 데이터 업데이트
-  useEffect(() => {
-    if (detailRef.current) {
-      setDetailScrollRef(prev => ({ ...prev, container: detailRef }));
-    }
-  }, [setDetailScrollRef]);
-
+  // 데이터 조회 상태
   useEffect(() => {
     if (code) {
       setLayoutState(prev => ({ ...prev, dataStatus: status }));
@@ -100,6 +61,43 @@ export default function ProjectDetail() {
       }
     }
   }, [code, data, setDetailData, setLayoutState, status]);
+
+  // 프로젝트 상세 오픈 ; 버튼 or URL
+  useEffect(() => {
+    console.log(
+      `[Detail Container :: before] open(${layoutState.open}) / code(${code})`
+    );
+    // URL 또는 프로젝트 선택 클릭 모두 적용하여 열기/닫기
+    if (code) setLayoutState(prev => ({ ...prev, open: true }));
+    setOpen(layoutState.open && code ? "open" : "");
+  }, [code, layoutState.open, open, setLayoutState]);
+
+  // 상세 페이지 오픈 슬라이드 완료 상태 업데이트
+  useEffect(() => {
+    if (code && open === "open") {
+      const timer = setTimeout(() => {
+        setLayoutState(prev => ({ ...prev, openComplete: true }));
+      }, transTime.detail.sheetSlide);
+
+      return () => clearTimeout(timer);
+    }
+  }, [code, open, setLayoutState]);
+
+  // 스크롤 참조 데이터 업데이트
+  useEffect(() => {
+    if (detailRef.current) {
+      setDetailScrollRef(prev => ({ ...prev, container: detailRef }));
+    }
+  }, [setDetailScrollRef]);
+
+  // 페이지 이동 시, 참조 데이터 초기화
+  useEffect(() => {
+    if (!code && !layoutState.open) {
+      return () => {
+        setDetailScrollRef(prev => ({ ...prev, container: null }));
+      };
+    }
+  }, [code, layoutState.open, setDetailScrollRef]);
 
   return (
     <PDContainer className={`${open}`} ref={detailRef}>
