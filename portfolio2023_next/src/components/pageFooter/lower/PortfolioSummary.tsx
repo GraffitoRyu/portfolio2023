4534d9768cd105bdd199c9a2bd995d1588208887
graffitoryu <yth4135@naver.com>
2023-06-26 17:06:53 +0900

    푸터 포트폴리오 요약 내용 수정

--- portfolio2023_next/src/components/pageFooter/lower/PortfolioSummary.tsx
+++ portfolio2023_next/src/components/pageFooter/lower/PortfolioSummary.tsx
@@ -10,9 +10,10 @@ export default function PortfolioSummary() {
       <PortfolioDescription>
         경력 4+ 년 프론트엔드 개발자 / 웹 퍼블리셔 <br />
         류대현 프로필 포트폴리오. <br />
-        Next.js, Recoil, SCSS, Styled-components, Tailwind, GSAP, TypeScript,
-        React Query, Firebase Database 등을 활용하여 구축 후 Vercel을 통해 배포.
-        2023년 프로젝트.
+        경력 4+ 년 프론트엔드 개발자 / 웹 퍼블리셔 류대현 프로필 포트폴리오.
+        Figma로 레이아웃 디자인. Next.js, Recoil, SCSS, Styled-components,
+        Tailwind, GSAP, TypeScript, React Query, Firebase Database 등을 활용하여
+        구축. Vercel을 통해 배포. 2023년 프로젝트.
       </PortfolioDescription>
       <PortfolioCopyright>&copy; 2023</PortfolioCopyright>
     </PortfolioSummaryContainer>
