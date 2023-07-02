f9a4eac3ee4278bd3aa125d458f8fcf4d744843e
graffitoryu <yth4135@naver.com>
2023-07-02 19:03:53 +0900

    스크롤 컨테이너 높이 observe

--- portfolio2023_next/src/components/roots/ScrollContainer.tsx
+++ portfolio2023_next/src/components/roots/ScrollContainer.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ReactNode, useCallback, useRef } from "react";
+import { ReactNode, useCallback } from "react";
 import { useSetRecoilState } from "recoil";
 
 // types
@@ -11,18 +11,26 @@ import { StyledScrollContainer } from "@/styles/styled/components/Page";
 
 // style
 import { scrollRefState } from "@/states/scroll";
+import useResizeObserver from "@/hooks/useResizeObserver";
 
 export default function ScrollContainer({ children }: { children: ReactNode }) {
   const setScrollRef = useSetRecoilState<ScrollRefStateTypes>(scrollRefState);
-  const scrollContainerRef = useRef<HTMLDivElement | null>(null);
 
   const setRef = useCallback(
     (node: HTMLDivElement | null) => {
-      scrollContainerRef.current = node;
-      setScrollRef(prev => ({ ...prev, container: node }));
+      setScrollRef(prev => ({
+        ...prev,
+        container: node,
+        height: node ? node.scrollHeight : 0,
+      }));
     },
     [setScrollRef]
   );
+  const scrollContainerRef = useResizeObserver(setRef);
 
-  return <StyledScrollContainer ref={setRef}>{children}</StyledScrollContainer>;
+  return (
+    <StyledScrollContainer ref={scrollContainerRef}>
+      {children}
+    </StyledScrollContainer>
+  );
 }
