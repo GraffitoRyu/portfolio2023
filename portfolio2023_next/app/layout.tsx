32c156715f30880bfa84d5b6f7b771a655b50460
graffitoryu <yth4135@naver.com>
2023-07-17 13:51:29 +0900

    font 최적화

--- portfolio2023_next/app/layout.tsx
+++ portfolio2023_next/app/layout.tsx
@@ -15,7 +15,7 @@ import InitPageCover from "@/components/pageInitialize/InitPageCover";
 import Cursor from "@/components/cursor/Cursor";
 
 // styles
-// import { sans, serif } from "@/styles/fonts/fonts"; // error로 사용할 수 없다
+import { sans, serif, serif_dm } from "@/styles/fonts/fonts"; // error로 사용할 수 없다
 import "@/styles/scss/globals.scss";
 import StyledComponentsRegistry from "./lib/registry";
 import { HTMLThemeStyle, PageContainer } from "@/styles/styled/components/Page";
@@ -25,16 +25,19 @@ import { PageLoadEvents } from "@/hooks/PageLoadEvents";
 import UpdateStateByResize from "@/hooks/UpdateStateByResize";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
-  // <html lang="ko" className={`${sans.variable} ${serif.variable}`}>
   return (
-    <html lang="ko">
-      <head>
-        {/* eslint-disable-next-line @next/next/no-page-custom-font */}
-        <link
-          href="https://fonts.googleapis.com/css2?family=DM+Serif+Display:ital@0;1&family=Noto+Sans+KR:wght@300;400;500;700;900&family=Noto+Serif+Display:ital,wght@0,700;1,500&family=Noto+Serif+KR:wght@200;400;500;700&display=swap"
+    <html
+      lang="ko"
+      className={`${sans.variable} ${serif.variable} ${serif_dm.variable}`}
+    >
+      {/* <html lang="ko"> */}
+      {/* <head> */}
+      {/* eslint-disable-next-line @next/next/no-page-custom-font */}
+      {/* <link
+          href="https://fonts.googleapis.com/css2?family=DM+Serif+Display:ital@0;1&family=Noto+Sans+KR:wght@300;400;500;700;900&family=Noto+Serif+KR:wght@200;400;500;700&display=swap"
           rel="stylesheet"
         />
-      </head>
+      </head> */}
       <body>
         <RecoilContainer>
           <ReactQueryContainer>
--- portfolio2023_next/src/styles/fonts/fonts.ts
+++ portfolio2023_next/src/styles/fonts/fonts.ts
@@ -1,16 +1,31 @@
-import { Noto_Sans_KR, Noto_Serif_KR } from "next/font/google";
+import {
+  DM_Serif_Display,
+  Noto_Sans_KR,
+  Noto_Serif_KR,
+} from "next/font/google";
 
 export const sans = Noto_Sans_KR({
   weight: ["300", "400", "500", "700", "900"],
   subsets: ["latin"],
-  preload: true,
   variable: "--sans-kr",
   display: "swap",
+  adjustFontFallback: false,
 });
+
 export const serif = Noto_Serif_KR({
   weight: ["400", "500", "700"],
   subsets: ["latin"],
   preload: true,
   variable: "--serif-kr",
   display: "swap",
+  adjustFontFallback: false,
+});
+
+export const serif_dm = DM_Serif_Display({
+  weight: ["400"],
+  style: "italic",
+  subsets: ["latin"],
+  variable: "--serif-dm",
+  display: "swap",
+  adjustFontFallback: false,
 });
