c8baab6b8230d86f57de2e356e3d1f8b8dc51b4c
GraffitoRyu <yth4135@naver.com>
2024-08-26 14:49:28 +0900

    [STYLE] 타입 주석 추가

--- portfolio2023_next/src/types/util/api.d.ts
+++ portfolio2023_next/src/types/util/api.d.ts
@@ -2,8 +2,19 @@
  * API Fetch URL 생성 유틸함수 props
  */
 type UtilGenerateApiUrlProps = {
+  /**
+   * Next.js api 라우트
+   * - 프론트 api url
+   */
   routeUrl: string;
+  /**
+   * 백엔드 api 라우트
+   * - 요청 쿼리 url
+   */
   queryUrl: string;
+  /**
+   * 로그에 출력할 데이터 객체
+   */
   log?: object;
 };
 
