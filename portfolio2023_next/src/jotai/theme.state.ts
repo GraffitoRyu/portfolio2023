e20a8d2fbd1bc9319bf8bf2010eecf644784baae
GraffitoRyu <yth4135@naver.com>
2024-09-03 17:41:47 +0900

    [FIX] 테마메뉴 에러 구성 - jotai atom 선언 로직 교정

--- portfolio2023_next/src/jotai/theme.state.ts
+++ portfolio2023_next/src/jotai/theme.state.ts
@@ -24,25 +24,30 @@ export const themeAtom = atomWithStorage<SystemThemeType>(
   localThemeStorage,
 );
 
+const themeManage = atom<SystemThemeStateTypes>({
+  isOpen: false, // page header; 테마 메뉴 오픈 여부
+  isSystem: true, // 현재 활성화된 테마가 시스템 테마인지의 여부
+  theme: "dark",
+});
+
 /**
  * 시스템 테마 상태관리
  * @state
  */
 export const themeState = atom(
-  get => ({
-    isOpen: false, // page header; 테마 메뉴 오픈 여부
-    isSystem: true, // 현재 활성화된 테마가 시스템 테마인지의 여부
-    theme: get(themeAtom),
-  }),
+  get => get(themeManage),
   (
     get,
     set,
     update: (props: SystemThemeStateTypes) => SystemThemeStateTypes,
   ) => {
-    const prev = get(themeState);
-    const d = update(prev);
+    const prev = get(themeManage);
+
+    const newState = update(prev);
+
+    set(themeManage, newState); // Update themeManage atom instead of themeState
+
     // themeState의 상태를 업데이트
-    set(themeAtom, d.theme); // themeAtom을 업데이트
-    set(themeState, () => d);
+    set(themeAtom, newState.theme); // themeAtom을 업데이트
   },
 );
