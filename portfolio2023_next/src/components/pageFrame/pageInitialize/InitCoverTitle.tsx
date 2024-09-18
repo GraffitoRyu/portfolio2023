d6406ae304468f4347f13111387c5f37325c615c
GraffitoRyu <yth4135@naver.com>
2024-09-18 17:25:47 +0900

    [REFACTOR] 진입 문구 수정

--- portfolio2023_next/src/components/pageFrame/pageInitialize/InitCoverTitle.tsx
+++ portfolio2023_next/src/components/pageFrame/pageInitialize/InitCoverTitle.tsx
@@ -4,7 +4,7 @@ import { StyledInitCoverTItle } from "@/styles/styled/components/InitPageCover";
 export default function InitTitle() {
   return (
     <StyledInitCoverTItle>
-      <span>FRONTEND DEVELOPER PORTFOLIO</span>
+      <span>Page initialize...</span>
     </StyledInitCoverTItle>
   );
 }
