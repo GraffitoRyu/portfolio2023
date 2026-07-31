248f90dfed0f67c4f06b27c0397f4cccf9ff94ec
GraffitoRyu <yth4135@naver.com>
2026-07-31 15:39:53 +0900

    [Build] Jotai atomFamily package 전환

--- apps/next/src/jotai/interaction/scroll.state.ts
+++ apps/next/src/jotai/interaction/scroll.state.ts
@@ -1,5 +1,5 @@
 import { atom } from "jotai";
-import { atomFamily } from "jotai/utils";
+import { atomFamily } from "jotai-family";
 
 /**
  * 스크롤 참조 ref 관리
--- apps/next/src/jotai/pages/project.detail.state.ts
+++ apps/next/src/jotai/pages/project.detail.state.ts
@@ -1,5 +1,5 @@
 import { atom } from "jotai";
-import { atomFamily } from "jotai/utils";
+import { atomFamily } from "jotai-family";
 
 /**
  * 프로젝트 각 상세 데이터 상태관리
